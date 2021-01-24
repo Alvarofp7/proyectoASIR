2d81b45f585806286ca6387b6e9071cb173a003c
Alvarofp7 <55496419+Alvarofp7@users.noreply.github.com>
2021-01-24 17:14:29 +0100

    Delete mandar_datos.php

--- html/datos/riego/mandar_datos.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "cabecera_conexionBD.php"
-$consulta = mysql_query("SELECT Humedad FROM Datos WHERE ID='4'";
-$resultado = $conexion -> query($consulta);
-$nombre = mysql_result($resultado, 0);
-echo "valor=" . $nombre . ";";
-?>
